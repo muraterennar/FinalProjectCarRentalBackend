1e1931aa91954b12a26b32ad2c713d5414f47759
Murat Eren Nar <muraterennar@hotmail.com>
2022-04-10 12:52:50 +0300

    10.04.2022 Commit 2

---
+++ Core/CrossCuttingConserns/Caching/ICacheManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConserns.Caching
+{
+    public interface ICacheManager
+    {
+        public interface ICacheManager
+        {
+            T Get<T>(string key);
+            object Get(string key);     //Üstekinin Alternatifi
+            void Add(string key, object data, int duration);
+            bool IsAdd(string key);
+            void Remove(string key);
+            void RemoveByPattern(string pattern);
+        }
+    }
+}
