90f1bb5f57d2b236ddc87fb0f438d884afc28faf
Murat Eren Nar <muraterennar@hotmail.com>
2022-04-10 12:27:16 +0300

    10.04.2022 Commit 1

---
+++ Core/Utilities/Helpers/FileHelper/FileHelperManager.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Helpers.FileHelper
+{
+    public class FileHelperManager : IFileHelper
+    {
+        public void Delete(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        public string Update(IFormFile file, string folder, string oldFolder)
+        {
+            Delete(Path.Combine(folder, oldFolder));
+            return Upload(file, folder);
+        }
+
+        public string Upload(IFormFile file, string folder)
+        {
+            string fileName = Guid.NewGuid().ToString() + "." + file.ContentType.Split("/")[1];
+            string fullPath = Path.Combine(folder, fileName);
+
+            using (FileStream fileStream = new FileStream(fullPath, FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+                fileStream.Flush();
+                return fileName;
+            }
+        }
+    }
+}
--- Core/Utilities/Results/IResult.cs
+++ Core/Utilities/Results/IResult.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.Utilities.Business
+namespace Core.Utilities.Results
 {
-    public interface IDataResult<T>:IResult
+    public interface IResult
     {
-        T Data { get; }
+        bool Success { get; }
+        string Message { get; }
     }
 }
