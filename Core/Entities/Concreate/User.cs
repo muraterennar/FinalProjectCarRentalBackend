69586c8954c0f6bc0663f64c140b903f6de2cf88
Murat Eren Nar <muraterennar@hotmail.com>
2022-04-28 17:35:17 +0300

    28.04.2022 Commit 1

--- Core/Entities/Concreate/User.cs
+++ Core/Entities/Concreate/User.cs
@@ -13,6 +13,7 @@ namespace Core.Entities.Concreate
         public DateTime Birthdate { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
+        public string Comment { get; set; }
         public byte[] PasswordSalt { get; set; }
         public byte[] PasswordHash { get; set; }
         public bool Status { get; set; }
